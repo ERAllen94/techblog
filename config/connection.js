c565aa13af9d3598d4d59db040b03f25f36f8731
Evan Allen <jacob_allen1994@yahoo.com>
2021-01-20 17:36:05 -0700

    added env

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME,process.envDB_USER,process.env.DB_PW, {
+    sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW, {
         host: 'localhost',
-        password: 'Corinthians10:13',
         dialect: 'mysql',
         port:3306
     });
